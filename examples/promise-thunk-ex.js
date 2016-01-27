ce68288367e4d75fed31b00d4bcd037cbcf1e420
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-01-28 01:06:48 +0900

    space to tab

--- examples/promise-thunk-ex.js
+++ examples/promise-thunk-ex.js
@@ -1,136 +1,136 @@
 // promise-thunk-ex.js
 
 (function () {
-  'use strict';
+	'use strict';
 
-  var PromiseThunk = require('../promise-thunk');
+	var PromiseThunk = require('../promise-thunk');
 
-  var slice = [].slice;
+	var slice = [].slice;
 
-  // normal node style callback
-  // returns: undefined
-  // format: sleep1(ms, ...args, cb)
-  function sleep1(ms) {
-    if (typeof ms !== 'number')
-      throw new TypeError('ms must be a number');
+	// normal node style callback
+	// returns: undefined
+	// format: sleep1(ms, ...args, cb)
+	function sleep1(ms) {
+		if (typeof ms !== 'number')
+			throw new TypeError('ms must be a number');
 
-    var args = slice.call(arguments, 1);
-    var cb = args.pop();
-    if (typeof cb !== 'function')
-      throw new TypeError('cb must be a function');
+		var args = slice.call(arguments, 1);
+		var cb = args.pop();
+		if (typeof cb !== 'function')
+			throw new TypeError('cb must be a function');
 
-    setTimeout.apply(null, [cb, ms, null].concat(args));
-  }
+		setTimeout.apply(null, [cb, ms, null].concat(args));
+	}
 
-  // returns thunk
-  // bug: no safety guard for duplicate calls and duplicate callbacks
-  // format: sleep2(ms, ...args)
-  function sleep2(ms) {
-    if (typeof ms !== 'number')
-      throw new TypeError('ms must be a number');
-    var args = arguments;
-    return function thunk(cb) {
-      args[args.length++] = cb;
-      sleep1.apply(null, args);
-    };
-  }
+	// returns thunk
+	// bug: no safety guard for duplicate calls and duplicate callbacks
+	// format: sleep2(ms, ...args)
+	function sleep2(ms) {
+		if (typeof ms !== 'number')
+			throw new TypeError('ms must be a number');
+		var args = arguments;
+		return function thunk(cb) {
+			args[args.length++] = cb;
+			sleep1.apply(null, args);
+		};
+	}
 
-  // returns thunk
-  // has safety guard for duplicate calls and duplicate callbacks
-  // format: sleep3(ms, ...args)
-  function sleep3(ms) {
-    if (typeof ms !== 'number')
-      throw new TypeError('ms must be a number');
+	// returns thunk
+	// has safety guard for duplicate calls and duplicate callbacks
+	// format: sleep3(ms, ...args)
+	function sleep3(ms) {
+		if (typeof ms !== 'number')
+			throw new TypeError('ms must be a number');
 
-    arguments[arguments.length++] = cb;
-    sleep1.apply(null, arguments);
+		arguments[arguments.length++] = cb;
+		sleep1.apply(null, arguments);
 
-    var callback, args;
+		var callback, args;
 
-    function cb() {
-      if (args) return; // already fired (callbacked)
-      args = arguments;
-      if (!callback) return;
-      callback.apply(null, args);
-    }
+		function cb() {
+			if (args) return; // already fired (callbacked)
+			args = arguments;
+			if (!callback) return;
+			callback.apply(null, args);
+		}
 
-    return function thunk(cb) {
-      if (callback) return; // already set callback function
-      callback = cb;
-      if (!args) return;
-      callback.apply(null, args)
-    };
-  }
+		return function thunk(cb) {
+			if (callback) return; // already set callback function
+			callback = cb;
+			if (!args) return;
+			callback.apply(null, args)
+		};
+	}
 
-  // returns promise/thenable
-  // format: sleep4(ms, ...args)
-  function sleep4(ms) {
-    if (typeof ms !== 'number')
-      throw new TypeError('ms must be a number');
+	// returns promise/thenable
+	// format: sleep4(ms, ...args)
+	function sleep4(ms) {
+		if (typeof ms !== 'number')
+			throw new TypeError('ms must be a number');
 
-    var args = slice.call(arguments);
+		var args = slice.call(arguments);
 
-    return new PromiseThunk(function (res, rej) {
-      sleep1.apply(null, args.concat(
-        function (err, val) {
-          if (err) rej(err);
-          else res(val); }));
-    });
-  }
+		return new PromiseThunk(function (res, rej) {
+			sleep1.apply(null, args.concat(
+				function (err, val) {
+					if (err) rej(err);
+					else res(val); }));
+		});
+	}
 
-  var sleep5 = PromiseThunk.wrap(sleep1);
+	var sleep5 = PromiseThunk.wrap(sleep1);
 
-  sleep1(1000, 'sleep1 ', function (err, val) { console.log('sleep1 ', err, val, 101); });
-  sleep2(1000, 'sleep2 ')(function (err, val) { console.log('sleep2 ', err, val, 201); });
-  sleep3(1000, 'sleep3 ')(function (err, val) { console.log('sleep3 ', err, val, 301); });
+	sleep1(1000, 'sleep1 ', function (err, val) { console.log('sleep1 ', err, val, 101); });
+	sleep2(1000, 'sleep2 ')(function (err, val) { console.log('sleep2 ', err, val, 201); });
+	sleep3(1000, 'sleep3 ')(function (err, val) { console.log('sleep3 ', err, val, 301); });
 
-  sleep4(1000, 'sleep4 ').then(
-    function (val) { console.log('sleep4 ', null, val, 401); },
-    function (err) { console.log('sleep4  err =', err, 402); });
+	sleep4(1000, 'sleep4 ').then(
+		function (val) { console.log('sleep4 ', null, val, 401); },
+		function (err) { console.log('sleep4  err =', err, 402); });
 
-  sleep5(1000, 'sleep5a')
-  (function (err, val) { console.log('sleep5a', err, val, 501); })
-  .catch(function (err) { console.log('sleep5a err =', err, 502); });
+	sleep5(1000, 'sleep5a')
+	(function (err, val) { console.log('sleep5a', err, val, 501); })
+	.catch(function (err) { console.log('sleep5a err =', err, 502); });
 
-  sleep5(1000, 'sleep5b').then(
-    function (val) { console.log('sleep5b', null, val, 505); },
-    function (err) { console.log('sleep5b err =', err, 506); });
+	sleep5(1000, 'sleep5b').then(
+		function (val) { console.log('sleep5b', null, val, 505); },
+		function (err) { console.log('sleep5b err =', err, 506); });
 
-  var p5 = sleep5(1000, 'sleep5 ')
-  (function (err, val) { console.log('sleep5u', err, val, 510); return 'sleep5u'; })
-  (function (err, val) { console.log('sleep5v', err, val, 520); return 'sleep5v'; })
-  (function (err, val) { console.log('sleep5w', err, val, 530); return 'sleep5w'; })
-  .then(
-    function (val) { console.log('sleep5x', null, val, 570); return 'sleep5x'; },
-    function (err) { console.log('sleep5x err =', err, 571); return 'sleep5xe'; })
-  .then(
-    function (val) { console.log('sleep5y', null, val, 580); return 'sleep5y'; },
-    function (err) { console.log('sleep5y err =', err, 581); return 'sleep5ye'; })
-  (function (err, val) { console.log('sleep5z', err, val, 599); return 'sleep5z'; })
-  ;
+	var p5 = sleep5(1000, 'sleep5 ')
+	(function (err, val) { console.log('sleep5u', err, val, 510); return 'sleep5u'; })
+	(function (err, val) { console.log('sleep5v', err, val, 520); return 'sleep5v'; })
+	(function (err, val) { console.log('sleep5w', err, val, 530); return 'sleep5w'; })
+	.then(
+		function (val) { console.log('sleep5x', null, val, 570); return 'sleep5x'; },
+		function (err) { console.log('sleep5x err =', err, 571); return 'sleep5xe'; })
+	.then(
+		function (val) { console.log('sleep5y', null, val, 580); return 'sleep5y'; },
+		function (err) { console.log('sleep5y err =', err, 581); return 'sleep5ye'; })
+	(function (err, val) { console.log('sleep5z', err, val, 599); return 'sleep5z'; })
+	;
 
-  if (typeof Promise !== 'undefined') {
-    console.log('', Promise.resolve('Promise.resolve'));
-    console.log('', new Promise(function (res, rej) { res('Promise.resolve'); }));
-    console.log('', Promise.reject(new Error('Promise.reject')));
-    console.log('', new Promise(function (res, rej) { rej(new Error('Promise.reject')); }));
-    console.log('', new Promise(function (res, rej) {}));
-  }
+	if (typeof Promise !== 'undefined') {
+		console.log('', Promise.resolve('Promise.resolve'));
+		console.log('', new Promise(function (res, rej) { res('Promise.resolve'); }));
+		console.log('', Promise.reject(new Error('Promise.reject')));
+		console.log('', new Promise(function (res, rej) { rej(new Error('Promise.reject')); }));
+		console.log('', new Promise(function (res, rej) {}));
+	}
 
-  function callback(err, val) {
-    if (err)
-      console.error('Catch: \x1b[41m%s\x1b[m', err);
-    else
-      console.info('Value: %s', val);
-  }
+	function callback(err, val) {
+		if (err)
+			console.error('Catch: \x1b[41m%s\x1b[m', err);
+		else
+			console.info('Value: %s', val);
+	}
 
-  if (typeof PromiseThunk !== 'undefined') {
-    console.log('', PromiseThunk.resolve('PromiseThunk.resolve'));
-    console.log('', new PromiseThunk(function (res, rej) { res('PromiseThunk.resolve'); }));
-    console.log('', PromiseThunk.reject(new Error('PromiseThunk.reject'))(callback));
-    console.log('', new PromiseThunk(function (res, rej) { rej(new Error('PromiseThunk.reject')); })(callback));
-    console.log('', new PromiseThunk(function (res, rej) { res('new PromiseThunk()'); }));
-    console.log('', new PromiseThunk(function (res, rej) {}));
-  }
+	if (typeof PromiseThunk !== 'undefined') {
+		console.log('', PromiseThunk.resolve('PromiseThunk.resolve'));
+		console.log('', new PromiseThunk(function (res, rej) { res('PromiseThunk.resolve'); }));
+		console.log('', PromiseThunk.reject(new Error('PromiseThunk.reject'))(callback));
+		console.log('', new PromiseThunk(function (res, rej) { rej(new Error('PromiseThunk.reject')); })(callback));
+		console.log('', new PromiseThunk(function (res, rej) { res('new PromiseThunk()'); }));
+		console.log('', new PromiseThunk(function (res, rej) {}));
+	}
 
 })();
--- examples/quick-example.js
+++ examples/quick-example.js
@@ -1,16 +1,16 @@
 try {
 var PromiseThunk = require('../promise-thunk');
 } catch (e) {
-  console.log('err: ' + e);
-  var PromiseThunk = require('promise-thunk');
+	console.log('err: ' + e);
+	var PromiseThunk = require('promise-thunk');
 }
 
 function sleepNodeStyle(msec, val, cb) {
-  console.log(val + ' timer start: ' + msec);
-  setTimeout(function () {
-    console.log(val + ' timer end  : ' + msec);
-    cb(null, val);
-  }, msec);
+	console.log(val + ' timer start: ' + msec);
+	setTimeout(function () {
+		console.log(val + ' timer end  : ' + msec);
+		cb(null, val);
+	}, msec);
 }
 
 var sleepPromiseThunk = PromiseThunk.wrap(sleepNodeStyle);
@@ -18,32 +18,32 @@ var sleepPromiseThunk = PromiseThunk.wrap(sleepNodeStyle);
 // promise type
 sleepPromiseThunk(1000, 'a1')
 .then(
-  function (val) {
-    console.log('a2 val: ' + val);
-    return sleepPromiseThunk(1000, 'a2'); },
-  function (err) { console.log('err:' + err); })
+	function (val) {
+		console.log('a2 val: ' + val);
+		return sleepPromiseThunk(1000, 'a2'); },
+	function (err) { console.log('err:' + err); })
 .catch(
-  function (err) { console.log('err:' + err); }
+	function (err) { console.log('err:' + err); }
 );
 
 // thunk type
 sleepPromiseThunk(1000, 'b1')
 (function (err, val) {
-  console.log('b2 val: ' + val + (err ? ' err: ' + err : ''));
-  return sleepPromiseThunk(1000, 'b2'); })
+	console.log('b2 val: ' + val + (err ? ' err: ' + err : ''));
+	return sleepPromiseThunk(1000, 'b2'); })
 (function (err, val) {
-  console.log('b3 val: ' + val + (err ? ' err: ' + err : '')); });
+	console.log('b3 val: ' + val + (err ? ' err: ' + err : '')); });
 
 // mixed promise and thunk type
 sleepPromiseThunk(1000, 'c1')
 (function (err, val) {
-  console.log('c2 val: ' + val + (err ? ' err: ' + err : ''));
-  return sleepPromiseThunk(1000, 'c2'); })
+	console.log('c2 val: ' + val + (err ? ' err: ' + err : ''));
+	return sleepPromiseThunk(1000, 'c2'); })
 .then(
-  function (val) {
-    console.log('c3 val: ' + val);
-    return sleepPromiseThunk(1000, 'c3'); },
-  function (err) { console.log('err:' + err); });
+	function (val) {
+		console.log('c3 val: ' + val);
+		return sleepPromiseThunk(1000, 'c3'); },
+	function (err) { console.log('err:' + err); });
 
 //output:
 // a1 timer start: 1000
