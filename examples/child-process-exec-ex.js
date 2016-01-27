6245a0300da91559908992278391c0d7fd4b2e86
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-01-27 21:13:55 +0900

    revise

--- examples/child-process-exec-ex.js
+++ examples/child-process-exec-ex.js
@@ -22,46 +22,58 @@
 	console.log('---- start');
 	var n = 0;
 
-	++n;
-	execThunk(cmd)(function (err, val) {
-		console.log(color(32) + '---- thunk   type:%s, class:%s', typeof val, val && val.constructor.name);
-		console.log(val);
-		console.log('---- thunk' + color());
-		end();
-	});
-	++n;
-	execThunk(cmd)(function (err, stdout, stderr) {
-		console.log(color(32) + '---- thunk   type:%s, stdout:%j', typeof stdout, stdout);
-		console.log('---- thunk   type:%s, stderr:%j', typeof stderr, stderr);
-		console.log('---- thunk' + color());
-		end();
-	});
+	setTimeout(function () {
+		++n;
+		execThunk(cmd)(function (err, val) {
+			console.log(color(32) + '---- thunk   type:%s, class:%s', typeof val, val && val.constructor.name);
+			console.log(val);
+			console.log('---- thunk' + color());
+			end();
+		});
+	}, 10);
 
-	++n;
-	execPromise(cmd).then(function (val) {
-		console.log(color(36) + '---- promise type:%s, class:%s', typeof val, val && val.constructor.name);
-		console.log(val);
-		console.log('---- promise' + color());
-	},
-	function (err) {
-		console.log(color(36) + '---- promise error', err);
-		console.log('---- promise' + color());
-	}).then(end);
+	setTimeout(function () {
+		++n;
+		execThunk(cmd)(function (err, stdout, stderr) {
+			console.log(color(32) + '---- thunk   type:%s, stdout:%j', typeof stdout, stdout);
+			console.log('---- thunk   type:%s, stderr:%j', typeof stderr, stderr);
+			console.log('---- thunk' + color());
+			end();
+		});
+	}, 20);
 
-	++n;
-	execPromise(cmd)(function (err, val) {
-		console.log(color(35) + '---- promise type:%s, class:%s',typeof val, val && val.constructor.name);
-		console.log(err ? err : '', val);
-		console.log('---- promise' + color());
-		end();
-	});
-	++n;
-	execPromise(cmd)(function (err, stdout, stderr) {
-		console.log(color(35) + '---- promise  type:%s, stdout:%j', typeof stdout, stdout);
-		console.log('---- promise type:%s, stderr:%j', typeof stderr, stderr);
-		console.log('---- promise' + color());
-		end();
-	});
+	setTimeout(function () {
+		++n;
+		execPromise(cmd).then(function (val) {
+			console.log(color(36) + '---- promise type:%s, class:%s', typeof val, val && val.constructor.name);
+			console.log(val);
+			console.log('---- promise' + color());
+		},
+		function (err) {
+			console.log(color(36) + '---- promise error', err);
+			console.log('---- promise' + color());
+		}).then(end);
+	}, 30);
+
+	setTimeout(function () {
+		++n;
+		execPromise(cmd)(function (err, val) {
+			console.log(color(35) + '---- promise type:%s, class:%s',typeof val, val && val.constructor.name);
+			console.log(err ? err : '', val);
+			console.log('---- promise' + color());
+			end();
+		});
+	}, 40);
+
+	setTimeout(function () {
+		++n;
+		execPromise(cmd)(function (err, stdout, stderr) {
+			console.log(color(35) + '---- promise  type:%s, stdout:%j', typeof stdout, stdout);
+			console.log('---- promise type:%s, stderr:%j', typeof stderr, stderr);
+			console.log('---- promise' + color());
+			end();
+		});
+	}, 50);
 
 	function end() {
 		if (--n === 0) console.log('---- end');
